e282d8c80fb557fa37d62510a2d90d31d9e22aae
Inory <dawnbreakFallenSky@foxmail.com>
2022-06-07 20:34:00 +0800

    完成编写dfsStatus.cpp
    2022.6.7
    1.完成编写dfsStatus.cpp
    by Inory

--- DFS_Kernel/APP/dfsStatus.cpp
+++ DFS_Kernel/APP/dfsStatus.cpp
@@ -26,4 +26,124 @@
 **                                         Include
 **-----------------------------------------------------------------------------------------
 */
+#include "dfsStatus.h"
 
+/*-----------------------------------------------------------------------------------------
+**                                   Function Definition
+**-----------------------------------------------------------------------------------------
+*/
+/*.BH--------------------------------------------------------------------------------------
+**
+** 函数名(Function Name): DfsStatus
+**
+** 描述(Description): 本函数为DfsStatus类构造函数
+**
+** 输入参数(Input Parameter):
+**      initValue , int 状态初始值
+**
+** 输出参数(Output Parameter):
+**
+** 返回值(Return Value):
+**
+** 设计注记(Design Annotation):
+**
+** 更改历史(Modification History):
+**      Inory, 2022年6月6日 创建本函数
+**      $Log$
+**
+**.EH--------------------------------------------------------------------------------------
+*/
+DfsStatus::DfsStatus(int initValue)
+{
+    _value = initValue;
+}
+/* END of DfsStatus */
+
+/*.BH--------------------------------------------------------------------------------------
+**
+** 函数名(Function Name): ~DfsStatus
+**
+** 描述(Description): 本函数为DfsStatus类析构函数
+**
+** 输入参数(Input Parameter):
+**
+** 输出参数(Output Parameter):
+**
+** 返回值(Return Value):
+**
+** 设计注记(Design Annotation):
+**
+** 更改历史(Modification History):
+**      Inory, 2022年6月6日 创建本函数
+**      $Log$
+**
+**.EH--------------------------------------------------------------------------------------
+*/
+DfsStatus::~DfsStatus()
+{
+#ifdef DEBUG
+    printf("DfsStatus::~DfsStatus()\n");
+#endif
+}
+/* END of ~DfsStatus */
+
+/*.BH--------------------------------------------------------------------------------------
+**
+** 函数名(Function Name): operator=
+**
+** 描述(Description): 本函数实现赋值运算符重载
+**
+** 输入参数(Input Parameter):
+**      value , int 状态值
+**
+** 输出参数(Output Parameter):
+**
+** 返回值(Return Value):
+**      Status& 对象引用
+**
+** 设计注记(Design Annotation):
+**
+** 更改历史(Modification History):
+**      Inory, 2022年6月6日 创建本函数
+**      $Log$
+**
+**.EH--------------------------------------------------------------------------------------
+*/
+Status& DfsStatus::operator=(int value)
+{
+    _mtx.lock();
+
+    _value = value;
+
+    _mtx.unlock();
+
+    return *this;
+}
+/* END of operator= */
+
+/*.BH--------------------------------------------------------------------------------------
+**
+** 函数名(Function Name): operator int
+**
+** 描述(Description): 本函数实现赋值运算符重载
+**
+** 输入参数(Input Parameter):
+**
+** 输出参数(Output Parameter):
+**
+** 返回值(Return Value):
+**      _value , int 状态值
+**
+** 设计注记(Design Annotation):
+**
+** 更改历史(Modification History):
+**      Inory, 2022年6月6日 创建本函数
+**      $Log$
+**
+**.EH--------------------------------------------------------------------------------------
+*/
+DfsStatus::operator int()
+{
+    return _value;
+}
+/* END of operator int */
